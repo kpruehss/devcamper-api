ca82fcbade45c2a42baf691c107bd87b43e151ff
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2019-10-26 15:04:51 +1100

    custom error handler
    Create custom error handler class to abstract sending JSON

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const colors = require('colors');
 const connectDB = require('./config/db');
 const bootcamps = require('./routes/bootcamps');
+const errorHandler = require('./middleware/error');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -25,6 +26,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
