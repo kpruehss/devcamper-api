0c2f6e8cc19e44019f800ea04c38a6a36d9f5dc8
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2019-10-26 14:46:45 +1100

    Crud Routes complete
    All routes connected and tested against MongoDB Atlas

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const connectDB = require('./config/db');
+const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -11,9 +12,11 @@ dotenv.config({ path: './config/config.env' });
 connectDB();
 
 // Route files
-const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
