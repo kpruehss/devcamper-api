1d8df44ee3c1f489d901b6068b1e2ce34a51f1ac
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2019-10-26 12:35:26 +1100

    connect mongoose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -24,7 +25,16 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold,
+  ),
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promse) => {
+  console.log(`Error: ${err.message}`.red);
+  // close server & exit process
+  server.close(() => process.exit(1));
+});
